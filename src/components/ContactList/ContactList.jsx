af47acdea41658c767fc4882093018695591f1f1
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-08-16 21:19:58 +0300

    components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Button } from 'style/style';
+
+export const ContactList = ({ data, children, deleteContact }) => {
+  return (
+    <>
+      <h2>Contacts</h2>
+      {children}
+      <ul onClick={deleteContact}>
+        {data.map(item => (
+          <li key={item.id}>
+            {' '}
+            {item.name}: {item.number}
+            <Button
+              type="button"
+              id={item.number}
+              onClick={() => deleteContact(item.id)}
+            >
+              Delete
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
